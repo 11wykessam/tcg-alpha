f6e240e2b3fe82bd9c8cf8f2322c85506ffd9a5f
11wykessam <samuelwykes@live.co.uk>
2023-10-07 15:16:06 +0100

    Initial commit, added Spring initializr stuff.

---
+++ src/main/java/com/wykessam/tsgalpha/TsgalphaApplication.java
@@ -0,0 +1,13 @@
+package com.wykessam.tsgalpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsgalphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsgalphaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wykessam/tsgalpha/TsgalphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wykessam.tsgalpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsgalphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
