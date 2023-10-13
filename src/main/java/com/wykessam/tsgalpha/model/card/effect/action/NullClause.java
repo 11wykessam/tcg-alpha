a502cad424d7183c5d406ba9f2fce411e0792641
11wykessam <samuelwykes@live.co.uk>
2023-10-13 18:45:13 +0100

    Added test cases for and and or clauses.

--- src/main/java/com/wykessam/tsgalpha/model/card/effect/action/NullClause.java
+++ src/main/java/com/wykessam/tsgalpha/model/card/effect/action/NullClause.java
@@ -5,6 +5,8 @@ import com.wykessam.tsgalpha.api.response.EffectResolutionResponseV2;
 import com.wykessam.tsgalpha.model.card.effect.ResolutionState;
 import reactor.core.publisher.Mono;
 
+import static com.wykessam.tsgalpha.model.card.effect.ResolutionState.RESOLVED;
+
 /**
  * @author Samuel Wykes.
  * Represents the terminating clause in an effect (i.e does nothing).
@@ -16,18 +18,36 @@ public class NullClause implements IActionClause {
         return "NULL";
     }
 
+    /**
+     * Get the current state of the clause's resolution.
+     *
+     * @return {@link ResolutionState}.
+     */
     @Override
     public ResolutionState getResolutionState() {
-        return null;
+        return RESOLVED;
     }
 
+    /**
+     * Resolve the clause.
+     * Returns a successful effect resolution.
+     *
+     * @param request {@link EffectResolutionRequestV1}.
+     * @return {@link EffectResolutionResponseV2}.
+     */
     @Override
-    public Mono<EffectResolutionResponseV2> resolve(EffectResolutionRequestV1 request) {
-        return null;
+    public Mono<EffectResolutionResponseV2> resolve(final EffectResolutionRequestV1 request) {
+        return Mono.just(EffectResolutionResponseV2.success());
     }
 
+    /**
+     * Reset the clause to the ready state.
+     * Nothing is needed here.
+     *
+     * @return {@link Void}.
+     */
     @Override
     public Mono<Void> reset() {
-        return null;
+        return Mono.empty();
     }
 }
--- src/test/java/com/wykessam/tsgalpha/model/card/effect/action/NullClauseTest.java
+++ src/test/java/com/wykessam/tsgalpha/model/card/effect/action/NullClauseTest.java
@@ -1,8 +1,14 @@
 package com.wykessam.tsgalpha.model.card.effect.action;
 
+import com.wykessam.tsgalpha.api.request.EffectResolutionRequestV1;
+import com.wykessam.tsgalpha.api.response.EffectResolutionResponseV2;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.test.StepVerifier;
 
+import static com.wykessam.tsgalpha.model.card.effect.ResolutionState.RESOLVED;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -10,5 +16,33 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @ExtendWith(MockitoExtension.class)
 class NullClauseTest {
+
+    @Mock
+    private EffectResolutionRequestV1 request;
+
+    @Test
+    void toStringTest() {
+        assertThat(getClause().toString())
+                .isEqualTo("NULL");
+    }
+
+    @Test
+    void resolve() {
+        StepVerifier.create(getClause().resolve(this.request))
+                .assertNext(response -> {
+                    assertThat(response).isEqualTo(EffectResolutionResponseV2.success());
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void getResolutionState() {
+        assertThat(getClause().getResolutionState())
+                .isEqualTo(RESOLVED);
+    }
+
+    private static NullClause getClause() {
+        return new NullClause();
+    }
 
 }
